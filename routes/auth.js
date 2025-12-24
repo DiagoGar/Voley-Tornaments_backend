beb56cab763eca2faaec4be7ab7fd6024bab36b5
Diago Garcia <diagogh6@gmail.com>
2025-12-24 20:52:21 -0300

    fix: cookie set

--- routes/auth.js
+++ routes/auth.js
@@ -29,7 +29,7 @@ router.post("/register", async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production", // en prod se usa https
-      sameSite: "strict",
+      sameSite: "none",
       maxAge: 24 * 60 * 60 * 1000, // 1 día
     });
 
@@ -64,7 +64,7 @@ router.post("/login", async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production", // usar HTTPS en producción
-      sameSite: "strict",
+      sameSite: "none",
       maxAge: 24 * 60 * 60 * 1000, // 1 día
     });
 
